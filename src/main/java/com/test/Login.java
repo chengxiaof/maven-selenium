efe0221e85af3069f633177455a875a0f7aa39ca
cxf <799304110@qq.com>
2018-07-27 20:03:14 +0800

    7-27

--- src/main/java/com/test/Login.java
+++ src/main/java/com/test/Login.java
@@ -12,6 +12,7 @@ public class Login {
 
     public WebDriver driver;
     public  WebElement element;
+    String filePath = "element.properties";
 
     public void InitDriver(){
         System.setProperty("webdriver.chrome.driver","chromedriver");
@@ -27,26 +28,22 @@ public class Login {
 
         String username = "15116999073";
         String password = "a.123456";
-        String id_jsbtn = "id";
-        String id_signup = "id";
-        String name_email = "name";
-        String name_password = "name";
-        String className_mocobtn = "className";
-        String id_header = "id";
-        String className = "name";
+        String id_jsbtn = "id_jsbtn";
+        String id_signup = "id_signup";
+        String name_email = "name_email";
+        String name_password = "name_password";
+        String className_mocobtn = "className_mocobtn";
+        String id_header = "id_header";
+        String classNames = "classNames";
 
 
-        String filePath = "element.properties";
-        ProUtil proUtil = new ProUtil(filePath);
-        String value = proUtil.getPro(id_jsbtn);
-
-        ByUserElement(id_jsbtn,value).click();
-        WebElement formElement = ByUserElement(id_signup,value);
-        WebElement user =ByUserElement(name_email,value);
+       /* ByUserElement(id_jsbtn1,id_jsbtn2).click();
+        WebElement formElement = ByUserElement(id_signup1,id_signup2);
+        WebElement user =ByUserElement(name_email1,name_email2);
         user.isDisplayed();
-        WebElement pwd = ByUserElement(name_password,value);
+        WebElement pwd = ByUserElement(name_password1,name_password2);
         pwd.isDisplayed();
-        WebElement loginButton = ByUserElement(className_mocobtn,value);
+        WebElement loginButton = ByUserElement(className_mocobtn1,className_mocobtn2);
         loginButton.isDisplayed();
         user.sendKeys(username);
         pwd.sendKeys(password);
@@ -55,11 +52,19 @@ public class Login {
         Actions actions =new Actions(driver);
         actions.moveToElement(header).perform();
         String name = ByUserElement(className,value).getText();
-        System.out.println(name);
+        System.out.println(name);*/
 
 
 
 
+    }
+
+    public void getValues(String param){
+
+        ProUtil proUtil = new ProUtil(filePath);
+        param = proUtil.getPro(param);
+        String param1=param.split(">")[0];
+        String param2=param.split(">")[1];
     }
 
     public  WebElement ByUserElement(String key,String name){
